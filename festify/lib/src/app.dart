9f0a83b277a3d306b3dd4070ff27915a3c86c23c
Thiago Alvarez <thiago.alvarez@hinosistemas.com.br>
2025-05-13 21:57:28 -0300

    Configurando Supabase

--- festify/lib/src/app.dart
+++ festify/lib/src/app.dart
@@ -1,4 +1,4 @@
-import 'package:festify/src/app_providers.dart';
+import 'package:festify/src/features/core/providers/app_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
