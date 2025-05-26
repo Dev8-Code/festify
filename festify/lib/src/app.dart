a60b9e7a5232b133e70c8ab7cb7ec1ef8df6d279
Igor Ferreira <igornsferreira@gmail.com>
2025-05-26 20:38:59 -0300

    [FEAT] Telas de cadastro de operadores com providers e layout

--- festify/lib/src/app.dart
+++ festify/lib/src/app.dart
@@ -1,13 +1,26 @@
 import 'package:festify/src/features/core/providers/app_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../src/features/supplier/presentation/register_supplier_page.dart';
+import '../src/features/operator/presentation/register_operator_page.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
 
-@override
+  @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
+      initialRoute: '/register-operator',
+      routes: {
+        // '/login': (context) => const LoginPage(),
+        //  '/password-reset': (context) => const PasswordResetPage(),
+        //  '/contract-main': (context) => const ContractMainPage(),
+        //   '/contract-details': (context) => const ContractDetailsPage(),
+
+        // Dev-Igor
+        '/register-operator': (context) => const RegisterOperatorPage(),
+        '/register-supplier': (context) => const RegisterSupplierPage(),
+      },
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: ref.watch(colorProvider),
@@ -15,11 +28,6 @@ class App extends ConsumerWidget {
               ref.watch(themeSwitchProvider)
                   ? Brightness.light
                   : Brightness.dark,
-        ),
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
         ),
       ),
     );
